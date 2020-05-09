386677ad80c8b9012dae1a4418f00b9b743f1b9d
Nick_SDB <shideboo@hotmail.com>
2020-05-09 09:35:13 +0800

    enable gpu

--- XXQG_V2_server.py
+++ XXQG_V2_server.py
@@ -19,7 +19,7 @@ if platform.machine() != 'x86_64':
     chrome_options = Options()
     # 无头模式启动
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("window-size=1024,768")
     chrome_options.add_argument("--no-sandbox")
     mybrowser = myWebdriver(chrome_options=chrome_options, executable_path="/usr/bin/chromedriver")
